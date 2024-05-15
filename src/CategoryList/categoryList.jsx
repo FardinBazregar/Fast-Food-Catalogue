60f38328faa249c5c88b59d79557353b4e744a18
Fardin Bazregar <fardin.mz01@gmail.com>
2024-05-15 23:52:57 +0330

    add loading

--- src/CategoryList/categoryList.jsx
+++ src/CategoryList/categoryList.jsx
@@ -1,39 +1,49 @@
 import { useEffect, useState } from "react";
 import axios from "../axios";
+import Loading from "../Loading/loading";
 
 const CategoryList = () => {
+  const [loading, setloading] = useState(true);
   const [categories, setCategories] = useState([]);
   useEffect(() => {
     const fetchCategories = async () => {
       const response = await axios.get("/FoodCategory/categories");
       setCategories(response.data);
+      setloading(false);
     };
     fetchCategories();
   }, []);
+
+  const renderContent = () => {
+    if (loading) {
+      return <Loading />;
+    }
+
+    return (
+        <ul className="nav">
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            همه فست فود ها
+          </a>
+        </li>
+        {categories.map((category) => (
+          <li className="nav-item" key={category.id}>
+            <a href="#" className="nav-link">
+              {category.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    )
+  };
+
   return (
     <nav className="container mt-n5">
       <div
         className=" d-flex align-items-center bg-white rounded-3 shadow-lg py-4"
         style={{ height: "80px" }}
       >
-
-
-    <ul className="nav">
-        <li className="nav-item">
-            <a href="#" className="nav-link">
-                همه فست فود ها
-            </a>
-        </li>
-        {categories.map((category=>(
-            <li className="nav-item" key={category.id}>
-                <a href="#" className="nav-link">
-                    {category.name}
-                </a>
-            </li>
-        )))}
-    </ul>
-
-
+        {renderContent()}
       </div>
     </nav>
   );
